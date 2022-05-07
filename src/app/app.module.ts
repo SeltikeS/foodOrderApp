7810dd46eed3e4a5346fcbd749da6501b854a541
SeltikeS <pschenka@gmail.com>
2022-05-07 22:29:18 +0300

    complite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RatingModule } from 'ng-starrating';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SearchComponent } from './search/search.component';
+import { TagsComponent } from './tags/tags.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { FoodpageComponent } from './foodpage/foodpage.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent,
+    TagsComponent,
+    CartPageComponent,
+    FoodpageComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RatingModule,
+    NgbModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
+import { Foods } from '../shared/models/food';
+import { StarRatingComponent } from 'ng-starrating';
+import { ActivatedRoute } from '@angular/router';
+import { identifierName } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  foods: Foods[] = [];
+  constructor(private fs: FoodService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      if (params['searchItem']) {
+        this.foods = this.fs
+          .getAll()
+          .filter((food) =>
+            food.name.toLowerCase().includes(params['searchItem'].toLowerCase())
+          );
+      } else if (params['tag']) {
+        this.foods = this.fs.getAllFoodByTag(params['tag']);
+      } else {
+        this.foods = this.fs.getAll();
+      }
+    });
   }
-
 }
